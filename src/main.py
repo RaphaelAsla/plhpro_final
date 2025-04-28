e0c1034cd801d1ccab71863245121c5f66b36dac
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-04-28 10:44:00 +0300

    some comment typos

--- src/main.py
+++ src/main.py
@@ -11,18 +11,16 @@ new_data = pd.read_excel("./../data/Project40NewCampaignData.xlsx")             
 
 if __name__ == "__main__":
     # auta epishs tha ginontai mesa sto GUI, o xrhsths tha mporei na kanei define monos toy ton aritho twn neighbors h an thelei na ginei aytomata to pick (find_best_neighbors)
-    # twra ta exw edw gia testing
     ########################################################################################################################################################################################################################
     knn = KNN() # <- neighbors go here (eg KNN(3))
     knn.feed_data(train_data) # <- pretty self explanatory
 
-    # mporoyme na valoyme kai custom ranges gia des (line 30 model.py) (pairnei ligh wra, an thes na testareis kalytera kanto me pre defined neighbors (eg KNN(3))
-    # obv den to kaloyme an exoyme kanei define tous neighbors monoi mas
+    # mporoyme na valoyme kai custom ranges gia neighbors (default 2 - 20) kai folds (2, 10), des (line 30 model.py) (pairnei ligh wra, an thes na testareis kalytera kanto me pre defined neighbors (eg KNN(3))
     knn.find_best_neighbors() # <- AN den exoyme thesei monoi mas tous neighbors (px predictor = KNN(3)), trexoyme ayto kai kanoyme me stoxo na vroyme to poio optimal (pio balanced synhthws, alla whatever)
 
     knn.fit() # <- training
 
-    predictions = knn.predict(new_data) # <- pretty self explanatory (returns dict)
+    predictions = knn.predict(new_data) # <- pretty self explanatory (returns pandas DataFrame)
     #predictions = knn.predict(new_data, output_path="./../predictions/predictions.xlsx") # <- mporoyme na ta kanoyme kai kateythian save
 
     knn.gen_metrics() # <- kanei generate ola ta metrics
@@ -31,6 +29,6 @@ if __name__ == "__main__":
     print(knn.validation_metrics_str) # <- ta idia metrics alla se ENA string an aplos thes na ta valeis se kapoio pedio sto gui
     ########################################################################################################################################################################################################################
 
-    # sto telos, mono ayta prepei na einai sto edw
+    # ta exw edw gia testing, sto telos, mono ayta prepei na vriskontai edw
     #gui = GUI(width, height, title)
     #gui.run()
